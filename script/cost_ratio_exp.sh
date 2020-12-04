bd2e05104d6a7bf0e47de42257f42f49f608a98a
ndhuynh <huynh.n.andy@gmail.com>
2020-12-04 00:26:19 -0500

    Adds cost function visualization notebook

---
+++ script/cost_ratio_exp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+BUILD_EXE="/Users/ndhuynh/projects/robust-lsm/rocksdb-robust/db_builder"
+RUN_EXE="/Users/ndhuynh/projects/robust-lsm/rocksdb-robust/db_runner"
+
+T=2
+K=1
+Z=1
+B=1048576
+E=1024
+BPE=5.0
+
